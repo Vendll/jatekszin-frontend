701363890b8f09d1bc29614c3a066c11edd6fd0e
Vendll <utto.vendel@icloud.com>
2023-08-21 14:57:14 +0200

    gallery

--- src/components/Galeria.svelte
+++ src/components/Galeria.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import { scrollRef } from 'svelte-scrolling';
+	import gallery1 from '$lib/assets/gallery1.jpg?format=webp';
+	import gallery2 from '$lib/assets/gallery2.jpg?format=webp';
+	import gallery3 from '$lib/assets/gallery3.jpg?format=webp';
+	import gallery4 from '$lib/assets/gallery4.jpg?format=webp';
+	import gallery5 from '$lib/assets/gallery5.jpg?format=webp';
+	import gallery6 from '$lib/assets/gallery6.jpg?format=webp';
+	import gallery7 from '$lib/assets/gallery7.jpg?format=webp';
+	import gallery8 from '$lib/assets/gallery8.jpg?format=webp';
+	import gallery9 from '$lib/assets/gallery9.jpg?format=webp';
+	import gallery10 from '$lib/assets/gallery10.jpg?format=webp';
+	import gallery11 from '$lib/assets/gallery11.jpg?format=webp';
+	import gallery12 from '$lib/assets/gallery12.jpg?format=webp';
 </script>
 
 <div use:scrollRef={'galéria'} class="m-6 space-y-4">
@@ -7,93 +19,46 @@
 	<section class="grid grid-cols-2 md:grid-cols-4 gap-2">
 		<div class="grid gap-4">
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery1} />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1597077962467-be16edcc6a43?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2MzZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery2} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1591775161903-497839a443c1?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3MzR8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery3} alt="" />
 			</div>
 		</div>
 		<div class="grid gap-4">
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1617296539691-67feaadf389e?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery4} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1542835435-4fa357baa00b?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NzN8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery5} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1553184570-557b84a3a308?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NTF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery6} alt="" />
 			</div>
 		</div>
 		<div class="grid gap-4">
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1603855873822-0931a843ee3a?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3ODJ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery7} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1509130446053-899ae7358ce6?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery8} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1508931133503-b1944a4ecdd5?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3OTV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery9} alt="" />
 			</div>
 		</div>
 		<div class="grid gap-4">
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1633053663400-655b31fb88ac?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MDV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery10} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1578170222009-c7893aa20afd?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MTZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery11} alt="" />
 			</div>
 			<div>
-				<img
-					class="h-auto max-w-full rounded-lg"
-					src="https://images.unsplash.com/photo-1610220941077-1ec123e7c043?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4NjV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max"
-					alt=""
-				/>
+				<img class="h-auto max-w-full rounded-lg" src={gallery12} alt="" />
 			</div>
 		</div>
 	</section>
