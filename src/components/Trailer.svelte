abbb67a49d25f75fcb8586f6818cb77f0affbc5d
Vendll <utto.vendel@icloud.com>
2023-08-21 14:37:39 +0200

    .

--- src/components/Trailer.svelte
+++ src/components/Trailer.svelte
@@ -4,7 +4,11 @@
 
 <div use:scrollRef={'trailer'} class="m-6 space-y-4">
 	<div class="font-heavitas !font-normal text-4xl mb-6">trailer</div>
-	<div class="w-full aspect-video bg-gray-300 rounded-md">a</div>
+	<div class="">
+		<div class="w-full aspect-video bg-gray-300 rounded-md flex">
+			<span class="justify-center">play</span>
+		</div>
+	</div>
 	<button
 		class="w-full py-2 px-4 bg-primary-500 text-2xl rounded-md font-heavitas text-white-stroke-thin text-error-500"
 		>megosztás</button
