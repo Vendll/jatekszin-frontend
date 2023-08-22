03254e75633a1472c6109388d1e78890c518bb56
Vendll <utto.vendel@icloud.com>
2023-08-22 11:08:37 +0200

    -

--- src/routes/(main)/hirek/[slug]/+page.svelte
+++ src/routes/(main)/hirek/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="relative overflow-hidden bg-white py-16">
+<div class="relative overflow-hidden bg-white py-16 mt-10">
 	<div
 		class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]"
 	/>
