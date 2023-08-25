9d6705d00d461796b7f40d8bb792c109443f09e4
SomogyiR69 <sroland97@gmail.com>
2023-08-25 13:23:22 +0200

    --

--- src/routes/(main)/eloadasok/+page.ts
+++ src/routes/(main)/eloadasok/+page.ts
@@ -2,7 +2,9 @@ import type { PageLoad } from './$types';
 import api from '$lib/api';
 
 export async function load() {
-	const { data } = await api.get('/eloadasok', { params: { limit: 10, page: 1, depth: 5 } });
+	const { data } = await api.get('/eloadasok', {
+		params: { limit: 10, page: 1, depth: 5, where: { archived: { equals: false } } }
+	});
 	const { data: pageMeta } = await api.get('/globals/eloadasok-oldal');
 
 	return {
